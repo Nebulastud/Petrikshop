cfffcbeda12f4e5b4d97b8ff87fe5653bf59ce38
Nebulastud <nuu0019@gmail.com>
2026-05-17 02:37:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -49,9 +49,9 @@ const products = [
    },
   {
     "id": 4,
-    "name": "𝙵𝙰𝙲𝙴𝙱𝙾𝙾𝙺 𝙾𝙻𝙳 𝙲𝙾𝙾𝙺𝙸𝙴𝚂",
+    "name": "𝙵𝙰𝙲𝙴𝙱𝙾𝙾𝙺 𝙾𝙻𝙳 𝙰𝟸𝙵 𝙾𝙽",
     "category": "𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊",
-    "price": 8000,
+    "price": 10000,
     "slug": "fb old cookies.html",
     "image": "facebook.png",
     "meta": "ᴀᴋᴜɴ ᴏʟᴅ ᴋᴜᴀʟɪᴛᴀꜱ ᴛᴇʀʙᴀɪᴋ ᴘᴇᴛʀɪᴋ ꜱʜᴏᴘ",
